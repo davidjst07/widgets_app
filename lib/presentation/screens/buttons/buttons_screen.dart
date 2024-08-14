ff87cdde75586df2b5c4fff25b98e0b958a47d75
davidjst07 <davidjst@hotmail.com>
2024-08-13 20:12:14 -0500

    button screen finish

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -70,7 +70,7 @@ class _ButtonsView extends StatelessWidget {
               label: const Text(' Text Icon'),
             ),
 
-            //TODO: custom button
+            const CustomButton(),
 
 
             IconButton(onPressed: () {}, icon: const Icon( Icons.app_registration_rounded)),
@@ -86,6 +86,30 @@ class _ButtonsView extends StatelessWidget {
 
           ]
         ,),
+      ),
+    );
+  }
+}
+
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+   final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric( horizontal: 20, vertical: 10),
+            child: Text('Hola Mundo', style: TextStyle(color: Colors.white),)),
+        ),
       ),
     );
   }
