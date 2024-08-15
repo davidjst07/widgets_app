4b085cef8ddc4addec5a0f569a220e60a9df54a8
davidjst07 <davidjst@hotmail.com>
2024-08-15 16:38:13 -0500

    cards finished

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 
 const cards = <Map<String,dynamic>>[
-  {'elevation': 0.0, 'Label': 'Elevation 0'},
-  {'elevation': 1.0, 'Label': 'Elevation 1'},
-  {'elevation': 2.0, 'Label': 'Elevation 2'},
-  {'elevation': 3.0, 'Label': 'Elevation 3'},
-  {'elevation': 4.0, 'Label': 'Elevation 4'},
-  {'elevation': 5.0, 'Label': 'Elevation 5'},
+  {'elevation': 0.0, 'label': 'Elevation 0'},
+  {'elevation': 1.0, 'label': 'Elevation 1'},
+  {'elevation': 2.0, 'label': 'Elevation 2'},
+  {'elevation': 3.0, 'label': 'Elevation 3'},
+  {'elevation': 4.0, 'label': 'Elevation 4'},
+  {'elevation': 5.0, 'label': 'Elevation 5'},
 ];
 
 
@@ -47,6 +47,16 @@ class _CardsView extends StatelessWidget {
           ...cards.map( 
             (card) => _CardType2( elevation: card['elevation'], label: card['label'] ),
           ),
+          
+          ...cards.map( 
+            (card) => _CardType3( elevation: card['elevation'], label: card['label'] ),
+          ),
+         
+          ...cards.map( 
+            (card) => _CardType4( elevation: card['elevation'], label: card['label'] ),
+          ),
+
+          const SizedBox(height: 100),
         ],
       ),
     );
@@ -133,6 +143,96 @@ class _CardType2 extends StatelessWidget {
           ],
         )
       ,),
+    );
+  }
+}
+
+
+class _CardType3 extends StatelessWidget {
+
+  final String label;
+  final double elevation;
+
+
+  const _CardType3({
+    required this.label,
+    required this.elevation
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.surfaceVariant,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed:() {},
+              ),
+            ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text('$label - Filed'),
+          )
+          ],
+        )
+      ,),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+
+  final String label;
+  final double elevation;
+
+
+  const _CardType4({
+    required this.label,
+    required this.elevation
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(
+        children: [
+      
+          Image.network(
+            'https://picsum.photos/id/${ elevation.toInt()}/600/350',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+      
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only( bottomLeft: Radius.circular(20))
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed:() {},
+              ),
+            ),
+          ),
+       
+        ],
+      ),
     );
   }
 }
