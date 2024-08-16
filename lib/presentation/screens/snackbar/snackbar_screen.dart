250def0b623f509e32d4a5ea3e096febc6531b63
davidjst07 <davidjst@hotmail.com>
2024-08-15 20:51:17 -0500

    defina la acción y desempeño de la snackbar

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -6,8 +6,33 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar( BuildContext context) {
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snakback = SnackBar(
+      content: const Text('Hola Mundo'),
+      action: SnackBarAction(label: 'Ok!', onPressed: (){}),
+      duration: const Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snakback);
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbars y diálogos'),
+      ),
+
+
+      floatingActionButton:  FloatingActionButton.extended(
+        label: const Text('Mostrar Snackbar'),
+        icon: const Icon( Icons.remove_red_eye_outlined),
+        onPressed: () => showCustomSnackbar(context),
+      )
+    );
   }
 }
