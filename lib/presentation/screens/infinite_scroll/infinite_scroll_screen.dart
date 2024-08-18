fd0146b20da15e0fd661e7a702ef014525bcb9f3
davidjst07 <davidjst@hotmail.com>
2024-08-18 16:20:46 -0500

    Implementación de infinite Scroll

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -15,7 +15,66 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
 
 
   List<int> imagesIds = [1,2,3,4,5];
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true;
 
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      
+      if( (scrollController.position.pixels + 500) >= scrollController.position.maxScrollExtent){
+
+        // Load next page
+        /* addFiveImages(); */
+        loadNextPage();
+      }
+      
+      //scrollController.position.maxScrollExtent;
+
+    });
+
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false;
+    super.dispose();
+  }
+
+
+  Future loadNextPage() async {
+
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    addFiveImages();
+    isLoading = false;
+
+    //TODO: revisar si está montado el componente / widget
+    if( !isMounted )return;
+
+    setState(() {});
+  }
+
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll(
+      [1,2,3,4,5].map((e) => lastId + e)
+    );
+
+  /*   setState(() {
+      
+    }); */
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +85,7 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
+          controller: scrollController,
           itemCount: imagesIds.length,
           itemBuilder: (context, index) {
             return FadeInImage(
