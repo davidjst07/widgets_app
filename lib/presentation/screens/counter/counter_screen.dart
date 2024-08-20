a0254656f31190423cb888d22bd4140a10f0cea5
davidjst07 <davidjst@hotmail.com>
2024-08-20 18:35:37 -0500

    Implementación de provider usando riverpod para cambio de iconos light y dark

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
 
@@ -13,11 +14,22 @@ class CounterScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
 
     final int clickCounter = ref.watch (counterProvider);
+    final bool isDarkmode = ref.watch(isDarkmodeProvider);
 
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pantalla de conteo'),
+        actions: [
+          IconButton(
+            //icon: Icon( Icons.light_mode_outlined),
+            icon: Icon( isDarkmode ? Icons.dark_mode_outlined : Icons.light_mode_outlined),
+            onPressed: (){
+              ref.read(isDarkmodeProvider.notifier)
+                .update((isDarkmode) => !isDarkmode);
+            }, 
+          )
+        ],
 
       ),
 
@@ -28,7 +40,8 @@ class CounterScreen extends ConsumerWidget {
       floatingActionButton:  FloatingActionButton(
         child: const Icon( Icons.add),
         onPressed: () {
-          
+          //ref.read(counterProvider.notifier).update((state) => state + 1);
+          ref.read(counterProvider.notifier).state++;
         },
       ),
     );
