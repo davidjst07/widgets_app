43b9fa44015cea3385c3f0bb4fca2a9f078b6a8a
davidjst07 <davidjst@hotmail.com>
2024-08-24 12:08:32 -0500

    Construido el theme changer screen

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,4 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 
 final isDarkmodeProvider = StateProvider((ref) => false);
 // Estado => isDarkmodeProvider = boolean
+
+// Listado de colores inmutable
+
+final colorListProvider = Provider((ref) => colorList);
